ad6a4f13cbeb481dee5a8f26e52dfa91acb817d1
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2023-06-02 18:07:38 +0900

    merge original master

--- src/py2dmat/solver/sigma.py
+++ src/py2dmat/solver/sigma.py
@@ -29,6 +29,9 @@ class Solver(py2dmat.solver.function.Solver):
         self._func = self._sigma_diff
         print(info.algorithm)
         self.alpha = info.algorithm["param"].get("alpha", 0.5)
+        self.sigma_experiment_mean_0 = np.mean(self.sigma_experiment[:,0])
+        self.sigma_experiment_mean_1 = np.mean(self.sigma_experiment[:,1])
+        
     def _sigma(self, B_target, xs):
         """
         xs : n_1, n_2, ..., n_N, mu_1, mu_2, ..., mu_N
@@ -52,10 +55,32 @@ class Solver(py2dmat.solver.function.Solver):
         sigma_xy = np.sum(sigma_xy_1, axis=1)
         return sigma_xx, sigma_xy
 
-    def _sigma_diff(self, xs: np.ndarray) -> float:
-        sigma_xx, sigma_xy = self._sigma(self.B_target, xs)
+    def _sigma_n_mu(self, B_target, xs):
+        """
+        xs : n_1, n_2, ..., n_N, mu_1, mu_2, ..., mu_N
+        sigma_xx (B) = \sum_{i=1}^N e n_i \mu_i / (1+\mu_i^2 B^2)
+        sigma_xy (B) = \sum_{i=1}^N sign(n_i) e n_i \mu_i^2 B/ (1+\mu_i^2 B^2)
+        Here, sign(s_i) = -1 if i <= N/2 (electron), 1 if i > N/2 (hole)
+        """
+        N = int(len(xs) / 2)
+        xs = np.array(xs)
+        en = xs[:N]
+        myu = xs[N:]
+        B_target = B_target.reshape(-1, 1)
 
-        delta_sigma_xx = np.sqrt(np.mean(((self.sigma_experiment[:,0] - sigma_xx)/np.mean(self.sigma_experiment[:,0]))**2))
-        delta_sigma_xy = np.sqrt(np.mean(((self.sigma_experiment[:,1] - sigma_xy)/np.mean(self.sigma_experiment[:,1]))**2))
+        # sigma_xxの計算
+        sigma_xx = np.sum(en * myu / (1 + myu ** 2 * B_target ** 2), axis=1)
+        # sigma_xyの計算
+        sigma_xy_1 = (-en[:int(N / 2)] * myu[:int(N / 2)] ** 2 / (1 + myu[:int(N / 2)] ** 2 * B_target ** 2)
+                      + en[int(N / 2):] * myu[int(N / 2):] ** 2 / (1 + myu[int(N / 2):] ** 2 * B_target ** 2)) * B_target
+        sigma_xy = np.sum(sigma_xy_1, axis=1)
+        return sigma_xx, sigma_xy
+
+    def _sigma_diff(self, xs: np.ndarray) -> float:
+        #sigma_xx, sigma_xy = self._sigma(self.B_target, xs)
+        sigma_xx, sigma_xy = self._sigma_n_mu(self.B_target, xs)
+
+        delta_sigma_xx = np.sqrt(np.mean(((self.sigma_experiment[:,0] - sigma_xx)/self.sigma_experiment_mean_0)**2))
+        delta_sigma_xy = np.sqrt(np.mean(((self.sigma_experiment[:,1] - sigma_xy)/self.sigma_experiment_mean_1)**2))
         alpha = self.alpha
         return alpha*delta_sigma_xx + (1.0-alpha)*delta_sigma_xy
